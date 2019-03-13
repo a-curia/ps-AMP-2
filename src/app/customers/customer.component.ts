6036e429763911162c8fc37146ca4f55c46ab85a
Gheorghe-Adrian Curia <gheorgheadrian.curia@ihsmarkit.com>
2019-03-13 08:49:18 +0200

    Adjusting Validation Rules at Runtime

--- src/app/customers/customer.component.ts
+++ src/app/customers/customer.component.ts
@@ -27,6 +27,8 @@ export class CustomerComponent implements OnInit {
       firstName: ['', [Validators.required, Validators.minLength(3)]],
       lastName: {value: 'n/a', disabled: true},
       email: '',
+      phone: '',
+      notification: 'email',
       sendCatalog: [true]
     });
   }
@@ -43,4 +45,18 @@ export class CustomerComponent implements OnInit {
     console.log(this.customerForm);
     console.log('Saved: ' + JSON.stringify(this.customerForm.value));
   }
+
+  setNotification(notifyVia: string): void {
+    const phoneControl = this.customerForm.get('phone');
+    if (notifyVia === 'text') {
+      phoneControl.setValidators(Validators.required);
+    } else {
+      phoneControl.clearValidators();
+    }
+
+    phoneControl.updateValueAndValidity();
+
+  }
+
+
 }
